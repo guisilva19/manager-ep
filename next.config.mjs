b120a953fc89ca3ce74a7079f8db4fc34216fbb9
unknown <2003silvagui@gmail.com>
2024-12-20 17:43:47 -0300

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "",
-        pathname: "**",
-      },
-    ],
+    // Remova a configuração de remotePatterns se não for necessária
   },
 
   async rewrites() {
