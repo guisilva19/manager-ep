c536985a52ac8346ae91cfd1860a186defd1a5b1
unknown <2003silvagui@gmail.com>
2024-12-20 19:14:44 -0300

    fix: url

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `https://electric-backend-production.up.railway.app/:path*`,
       },
     ];
   },
